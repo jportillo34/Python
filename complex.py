c3268fdd237a69703f6f8efc6c2da9c957c991da
Jose Portillo <jportillo34@gmail.com>
2024-08-02 12:10:50 +0200

    Nuevo fuente complex.py
    Se incluye manejo de errores. Lo habiamos olvidado.

---
+++ complex.py
@@ -0,0 +1,18 @@
+import sys
+
+class complejo:
+    def __init__(self, rI, jI):
+        self.r = rI
+        self.j = jI
+    def __add__(self, C):
+        return complejo(self.r + C.r, self.j + C.j)
+    def __sub__(self, C):
+        return complejo(self.r - C.r, self.j - C.j)
+    def __str__(self):
+        return "({},{})".format(self.r, self.j)
+
+if __name__ == "__main__":
+    P = complejo(3.0, 2.0)
+    Q = complejo(4.0, 4.0)
+    print("P + Q = {}".format(P + Q))
+    sys.exit(0)
