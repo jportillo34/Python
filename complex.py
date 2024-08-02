9766b6b4013de758cf6c8332bc26a87502835c20
Jose Portillo <37955235+jportillo34@users.noreply.github.com>
2024-08-02 18:01:46 +0200

    Se elimina el metodo __neg__ y se comenta el print en repo remoto

--- complex.py
+++ complex.py
@@ -8,13 +8,11 @@ class complejo:
         return complejo(self.r + C.r, self.j + C.j)
     def __sub__(self, C):
         return complejo(self.r - C.r, self.j - C.j)
-    def __neg__(self):
-        return complejo((-1)*self.r, (-1)*self.j)
     def __str__(self):
         return "({},{})".format(self.r, self.j)
 
 if __name__ == "__main__":
     P = complejo(3.0, 2.0)
     Q = complejo(4.0, 4.0)
-    print("P + Q = {}".format(P + Q))
+    # print("P + Q = {}".format(P + Q))
     sys.exit(0)
