fda83f43f1888354f716772884f0df9998fc1cb1
Jose Portillo <jportillo34@gmail.com>
2024-08-02 19:22:48 +0200

    Se cambia la indentacion y adicionan comentarios

--- complex.py
+++ complex.py
@@ -1,20 +1,25 @@
 import sys
 
+# Representa a un numero complejo.
 class complejo:
-    def __init__(self, rI, jI):
-        self.r = rI
-        self.j = jI
-    def __add__(self, C):
-        return complejo(self.r + C.r, self.j + C.j)
-    def __sub__(self, C):
-        return complejo(self.r - C.r, self.j - C.j)
-    def __str__(self):
-        return "({},{})".format(self.r, self.j)
+	def __init__(self, rI, jI):
+		self.r = rI
+		self.j = jI
+	def __add__(self, C):
+        	return complejo(self.r + C.r, self.j + C.j)
+	def __sub__(self, C):
+		return complejo(self.r - C.r, self.j - C.j)
+	def __neg__(self):
+		return complejo((-1)*self.r, (-1)*self.j)
+	def __str__(self):
+		return "({},{})".format(self.r, self.j)
 
+
+# Punto de entrada al programa.
 if __name__ == "__main__":
 try:
-P = complejo(3.0, 2.0)
-Q = complejo(4.0, 4.0)
+	P = complejo(3.0, 2.0)
+	Q = complejo(4.0, 4.0)
 		print("P + Q = {}".format(P + Q))
 	except:
 		print("Ocurrio un error")
