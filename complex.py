0be6b3bb568a0b1f315495699078db827354529b
Jose Portillo <jportillo34@gmail.com>
2024-08-03 10:24:52 +0200

    Se adiciona funcion principal main y mensajes de ayuda - repo Local

--- complex.py
+++ complex.py
@@ -1,7 +1,7 @@
 import sys
 
-# Representa a un numero complejo.
 class complejo:
+	""" Representa a un numero complejo """
 	def __init__(self, rI, jI):
 		self.r = rI
 		self.j = jI
@@ -14,14 +14,16 @@ class complejo:
 	def __str__(self):
 		return "({},{})".format(self.r, self.j)
 
-
-# Punto de entrada al programa.
-if __name__ == "__main__":
-try:
-	P = complejo(3.0, 2.0)
-	Q = complejo(4.0, 4.0)
+def main():
+	""" Funcion principal del programa """
+	try:
+		P = complejo(3.0, 2.0)
+		Q = complejo(4.0, 4.0)
 		print("P + Q = {}".format(P + Q))
 	except:
 		print("Ocurrio un error")
 		sys.exit(1)
 	sys.exit(0)
+
+if __name__ == "__main__":
+	main()
